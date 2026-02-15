4db88a860e651071d18171b832fe7d21467cbfe5
Biren Badrakiya <144122503+Biren06@users.noreply.github.com>
2026-02-15 21:24:48 +0530

    Create List component to display favorite foods

---
+++ componentspractice/ex1/src/List.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+export default function List() {
+  return (
+    <>
+      <h1>My Favourite Foods</h1>
+      <ul>
+        <li>Bacon</li>
+        <li>Jamon</li>
+        <li>Noodles</li>
+      </ul>
+    </>
+  );
+}
